85487a357dba597a4e393f5f49f0a9cc1fde5013
Ermiyas H <Ermi@Ermiyass-MacBook-Pro-2.local>
2023-02-28 20:25:26 -0800

    coding challenge q5-6

--- script.js
+++ script.js
@@ -124,3 +124,41 @@ computeRemainder(4,0) //=> Infinity
 computeRemainder(10.5, 3) //=> 1.5
 -----------------------------------------------------------------*/
 // Your solution for 05-computeRemainder:
+
+const computeRemainder = (num1, num2) => {
+    if (num2 === 0) {
+        return Infinity;
+    } else {
+        return num1 % num2;
+    }
+}
+console.log(computeRemainder(10, 2));
+// console.log(computeRemainder(4, 0));
+
+/*-----------------------------------------------------------------
+Challenge: 06-range
+Difficulty: basic
+Prompt:
+- Write a function called range that accepts two integers as arguments and returns an array of integers starting with the first argument up to one less than the second argument.
+- The range function must be called with the first argument less than or equal to the second argument, otherwise return the string "First argument must be less than second".
+Examples:
+range(1,4) //=> [1,2,3]
+range(-2, 3) //=> [-2,-1,0,1,2]
+range(1,1) //=> []
+range(5,2) //=> "First argument must be less than second"
+-----------------------------------------------------------------*/
+// Your solution for 06-range here:
+
+const range = (num1, num2) => {
+    let arr = [];
+    if (num1 > num2) {
+        return 'First argument must be less than second';
+    } else {
+        for (let i = num1; i < num2; i++) {
+            arr.push(i);
+        }
+    }
+    return arr;
+}
+console.log(range(1, 4));
+

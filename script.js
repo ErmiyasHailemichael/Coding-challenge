d7c42743906b6d947ba8dd00176d70a3ceefffcb
Ermiyas H <Ermi@Ermiyass-MacBook-Pro-2.local>
2023-02-28 20:21:45 -0800

    coding challenge q1-4

---
+++ script.js
@@ -0,0 +1,126 @@
+/*-----------------------------------------------------------------
+Challenge: 20-isPrime
+Difficulty: Intermediate
+Prompt:
+- Write a function named isPrime that returns true when the integer argument passed to it is a prime number and false when the argument passed to it is not prime.
+- A prime number is a whole number (integer) greater than 1 that is evenly divisible by only itself.
+Examples:
+isPrime(2) //=> true
+isPrime(3) //=> true 
+isPrime(4) //=> false
+isPrime(29) //=> true
+isPrime(200) //=> false
+-----------------------------------------------------------------*/
+// Your solution for 20-isPrime here:
+
+// const isPrime = (num) => {
+//     let prime = true;
+//     for (let i = 2; i < num; i++) {
+//         if (num % i === 0) {
+//         prime = false;
+//         }
+//     }
+//     return prime;
+//     }
+//     console.log(isPrime(10));
+
+/*-----------------------------------------------------------------
+Challenge: 01-addOne
+Difficulty: Basic
+Prompt:
+Write a function called addOne that takes a single number as an argument and returns that number plus 1.
+Examples:
+addOne(1) //=> 2
+addOne(-5) //=> -4
+-----------------------------------------------------------------*/
+// Your solution for 01-addOne here:
+
+const addOne = (num) => {
+    return num + 1;
+}
+console.log(addOne(-5));
+
+/*-----------------------------------------------------------------
+Challenge: 02-addTwoNumbers
+Difficulty: Basic  
+Prompt:
+Write a function called addTwoNumbers that accepts two numeric arguments and returns the sum of those two numbers.
+If either argument is not a Number, return the value of NaN.
+Examples:
+addTwoNumbers(5, 10) //=> 15
+addTwoNumbers(10, -2) //=> 8
+addTwoNumbers(0, 0) //=> 0
+addTwoNumbers('Hello', 5) //=> NaN
+-----------------------------------------------------------------*/
+// Your solution for 02-addTwoNumbers here:
+
+const addTwoNumbers = (num1, num2) => {
+    if (typeof num1 === 'number' && typeof num2 === 'number') {
+        return num1 + num2;
+    } else {
+        return NaN;
+    }
+}
+console.log(addTwoNumbers(5, 10));
+
+/*-----------------------------------------------------------------
+Challenge: 03-sumNumbers
+Difficulty: Basic  
+Prompt:
+- Write a function called sumNumbers that accepts a single array of numbers and returns the sum of the numbers in the array.
+- If the array is empty, return 0 (zero).
+Examples:
+sumNumbers([10]) //=> 10
+sumNumbers([5, 10]) //=> 15
+sumNumbers([2, 10, -5]) //=> 7
+sumNumbers([]) //=> 0
+-----------------------------------------------------------------*/
+// Your solution for 03-sumNumbers here:
+
+const sumNumbers = (arr) => {
+    let sum = 0;
+    for (let i = 0; i < arr.length; i++) {
+        sum += arr[i];
+    }
+    return sum;
+}
+console.log(sumNumbers([2, 10, -5]));
+/*-----------------------------------------------------------------
+Challenge: 04-addList
+Difficulty: Basic
+Prompt:
+- Write a function called addList that accepts any quantity of numbers as arguments, adds them together and returns the resulting sum.
+- Assume all parameters will be numbers.
+- If called with no arguments, return 0 (zero).
+Examples:
+add(1) //=> 1
+add(1, 50, 1.23) //=> 52.23
+add(7, -12) //=> -5
+
+Hint:  Check out the Further Study section of the JS Functions lesson regarding "rest parameters"
+-----------------------------------------------------------------*/
+// Your solution for 04-addList here:
+
+const addList = (...args) => {  
+    let sum = 0;
+    for (let i = 0; i < args.length; i++) {
+        sum += args[i];
+    }
+    return sum;
+}
+console.log(addList(1, 50, 1.23));
+
+/*-----------------------------------------------------------------
+Challenge: 05-computeRemainder
+Difficulty: Basic
+Prompt:
+- Write a function named computeRemainder that accepts two numeric arguments and returns the remainder of the division of those two numbers.
+- The first argument should be the dividend and the second argument should be the divisor.
+- If a 0 is passed in as the second argument you should return JavaScript's special numeric value: Infinity.
+- For extra fun, complete this challenge without using the modulus (%) operator.
+Examples:
+computeRemainder(10,2) //=> 0
+computeRemainder(4,0) //=> Infinity
+computeRemainder(10.5, 3) //=> 1.5
+-----------------------------------------------------------------*/
+// Your solution for 05-computeRemainder:

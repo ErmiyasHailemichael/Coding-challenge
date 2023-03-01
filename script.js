23f2b4895e6dc7b569830e824e9b7967c6316f2d
Ermiyas H <Ermi@Ermiyass-MacBook-Pro-2.local>
2023-02-28 20:31:05 -0800

    coding challenge q5-7

--- script.js
+++ script.js
@@ -162,3 +162,22 @@ const range = (num1, num2) => {
 }
 console.log(range(1, 4));
 
+/*-----------------------------------------------------------------
+Challenge: 07-reverseUpcaseString
+Difficulty: Basic
+Prompt:
+- Write a function called reverseUpcaseString that accepts a single string argument, then returns the string with its characters in reverse order and converts all characters to uppercase.
+Examples:
+reverseUpcaseString("SEI Rocks!"); //=> "!SKCOR IES" 
+-----------------------------------------------------------------*/
+// Your solution for 07-reverseUpcaseString here:
+
+function reverseUpcaseString(str) {
+    return str.split('').reverse().join('').toUpperCase();
+}
+console.log(reverseUpcaseString('SEI Rocks!'));
+
+// const reverseUpcaseString = (str) => {
+//     return str.split('').reverse().join('').toUpperCase();
+// }
+// console.log(reverseUpcaseString('SEI Rocks!'));

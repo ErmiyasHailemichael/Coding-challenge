3df18eec26812187a1d737cd3d24a79d3fffca53
Ermiyas H <Ermi@Ermiyass-MacBook-Pro-2.local>
2023-02-28 20:38:44 -0800

    coding challenge q6-10

--- script.js
+++ script.js
@@ -207,3 +207,56 @@ function removeEnds(str) {
     }
 }
 console.log(removeEnds('SEI Rocks!'));
+
+/*-----------------------------------------------------------------
+Challenge: 09-charCount
+Difficulty: Basic
+Prompt:
+- Write a function named charCount that accepts a single string argument and returns an object that represents the count of each character in the string.
+- The returned object should have keys that represent the character with its value set to the how many times the character appears in the string argument.
+- Upper and lower case characters should be counted separately.
+- Space characters should be count too.
+Examples:
+charCount('hello') //=> { h: 1, e: 1, l: 2, o: 1 }
+charCount('Today is fantastic!') //=> { T: 1, o: 1, d: 1, a: 3, y: 1, ' ': 2, i: 2, s: 2, f: 1, n: 1, t: 2, c: 1, '!': 1 }
+-----------------------------------------------------------------*/
+// Your solution for 09-charCount here:
+
+function charCount(str) {
+    let obj = {};
+    for (let i = 0; i < str.length; i++) {
+        if (obj[str[i]] === undefined) {
+            obj[str[i]] = 1;
+        } else {
+            obj[str[i]]++;
+        }
+    }
+    return obj;
+}
+console.log(charCount('Today is fantastic!'));
+
+/*-----------------------------------------------------------------
+Challenge: 10-formatWithPadding
+Difficulty: Basic
+Prompt:
+- Write a function called formatWithPadding that accepts three arguments:
+  - A numeric argument (an integer) representing the number to format.
+  - A string argument (a single character) representing the character used to "pad" the returned string to a minimum length.
+  - Another numeric argument (an integer) representing the length to "pad" the returned string to.
+- The function should return the integer as a string, "left padded" to the length of the 3rd arg using the character provided in the 2nd arg.
+- If the length of the integer converted to a string is equal or greater than the 3rd argument, no padding is needed - just return the integer as a string.
+Examples:
+formatWithPadding(123, '0', 5); //=> "00123"
+formatWithPadding(42, '*', 10); //=> "********42"
+formatWithPadding(1234, '*', 3); //=> "1234"
+-----------------------------------------------------------------*/
+// Your solution for 10-formatWithPadding here:
+
+function formatWithPadding(num, str, num2) {
+    if (num.toString().length >= num2) {
+        return num.toString();
+    } else {
+        return str.repeat(num2 - num.toString().length) + num.toString();
+    }
+}
+console.log(formatWithPadding(123, '0', 5));

96bbfd7a27c624abfd8c998530edac029a349068
Ermiyas H <Ermi@Ermiyass-MacBook-Pro-2.local>
2023-02-28 20:34:47 -0800

    coding challenge q6-8

--- script.js
+++ script.js
@@ -40,6 +40,11 @@ const addOne = (num) => {
 }
 console.log(addOne(-5));
 
+// function addOne(num) {
+//     return num + 1;
+// }
+// console.log(addOne(-5));
+
 /*-----------------------------------------------------------------
 Challenge: 02-addTwoNumbers
 Difficulty: Basic  
@@ -181,3 +186,24 @@ console.log(reverseUpcaseString('SEI Rocks!'));
 //     return str.split('').reverse().join('').toUpperCase();
 // }
 // console.log(reverseUpcaseString('SEI Rocks!'));
+
+/*-----------------------------------------------------------------
+Challenge: 08-removeEnds
+Difficulty: Basic
+Prompt:
+- Write a function called removeEnds that accepts a single string argument, then returns the a string with the first and last characters removed.
+- If the length of the string argument is less than 3, return an empty string.
+Examples:
+removeEnds('SEI Rocks!'); //=> "EI Rocks"
+removeEnds('a'); //=> "" (empty string)
+-----------------------------------------------------------------*/
+// Your solution for 08-removeEnds here:
+
+function removeEnds(str) {
+    if (str.length < 3) {
+        return '';
+    } else {
+        return str.slice(1, -1);
+    }
+}
+console.log(removeEnds('SEI Rocks!'));
